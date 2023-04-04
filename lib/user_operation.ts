05079c8d348d3618fd1bf6d6d4018f0a63d1e37a
linkdrone <fengwanqing2014@gmail.com>
2023-04-04 11:08:28 +0800

    fixed: bugs

--- lib/user_operation.ts
+++ lib/user_operation.ts
@@ -1,18 +1,18 @@
-import { BigNumberish, BytesLike, constants } from "ethers";
+import { BigNumberish, BytesLike, constants } from "ethers"
 
 export type UserOperationStruct = {
-  sender: string;
-  nonce: BigNumberish;
-  initCode: BytesLike;
-  callData: BytesLike;
-  callGasLimit: BigNumberish;
-  verificationGasLimit: BigNumberish;
-  preVerificationGas: BigNumberish;
-  maxFeePerGas: BigNumberish;
-  maxPriorityFeePerGas: BigNumberish;
-  paymasterAndData: BytesLike;
-  signature: BytesLike;
-};
+  sender: string
+  nonce: BigNumberish
+  initCode: BytesLike
+  callData: BytesLike
+  callGasLimit: BigNumberish
+  verificationGasLimit: BigNumberish
+  preVerificationGas: BigNumberish
+  maxFeePerGas: BigNumberish
+  maxPriorityFeePerGas: BigNumberish
+  paymasterAndData: BytesLike
+  signature: BytesLike
+}
 
 export const defaultsForUserOp: UserOperationStruct = {
   sender: constants.AddressZero,
@@ -20,10 +20,10 @@ export const defaultsForUserOp: UserOperationStruct = {
   initCode: "0x",
   callData: "0x",
   callGasLimit: 0,
-  verificationGasLimit: 50000,
-  preVerificationGas: 50000,
+  verificationGasLimit: 50,
+  preVerificationGas: 50,
   maxFeePerGas: 0,
   maxPriorityFeePerGas: 1e9,
   paymasterAndData: "0x",
   signature: "0x",
-};
+}
